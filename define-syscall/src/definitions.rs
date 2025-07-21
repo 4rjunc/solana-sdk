261bb88f39e0c9392d4a812ee122b54804398660
4rjunc <arjunmundamani@gmail.com>
2025-07-21 11:46:07 +0530

    test: defined the custom syscall function

--- define-syscall/src/definitions.rs
+++ define-syscall/src/definitions.rs
@@ -28,6 +28,9 @@ define_syscall!(fn sol_curve_pairing_map(curve_id: u64, point: *const u8, result
 define_syscall!(fn sol_alt_bn128_group_op(group_op: u64, input: *const u8, input_size: u64, result: *mut u8) -> u64);
 define_syscall!(fn sol_big_mod_exp(params: *const u8, result: *mut u8) -> u64);
 define_syscall!(fn sol_remaining_compute_units() -> u64);
+// CUSTOM SYSCALL
+define_syscall!(fn sol_get_magic_number() -> u64);
+// CUSTOM SYSCALL
 define_syscall!(fn sol_alt_bn128_compression(op: u64, input: *const u8, input_size: u64, result: *mut u8) -> u64);
 define_syscall!(fn sol_get_sysvar(sysvar_id_addr: *const u8, result: *mut u8, offset: u64, length: u64) -> u64);
 define_syscall!(fn sol_get_epoch_stake(vote_address: *const u8) -> u64);
